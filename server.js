86fff5421f0ec1376d2bb4517ffb9d4170e3d825
NimeshHasaranga <it22140852@my.sliit.lk>
2026-03-03 21:52:02 +0530

    Folder-Structure
    Full folder structure added

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const connectDB = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
+
+const swaggerDoc = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`🚀 Server running on port ${process.env.PORT}`)
+);
